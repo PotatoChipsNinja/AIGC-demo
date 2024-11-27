0ee30f0de6f33da55918089bfd2f33131243a04c
Yuhui Shi <shiyuhui2000@126.com>
2024-11-27 18:10:19 +0800

    improve feature name readability

--- src/components/Full.vue
+++ src/components/Full.vue
@@ -13,6 +13,7 @@ const isClear = ref(true)
 const isLoading = ref(false)
 const reportVisible = ref(false)
 const time = ref('')
+const featureDesc = ref(["", "平均每词字符数", "平均每句词数", "平均每句单字词数", "平均每句双字词数", "平均每句三字以上词数", "停用词占词数比例", "独特词数比例", "平均每句标点符号数", "平均每句逗号数", "逗号数占比", "平均每句句号数", "句号数占比", "平均每句冒号数", "冒号数占比", "平均每句分号数", "分号数占比", "平均每句问号数", "问号数占比", "平均每句感叹号数", "感叹号数占比", "平均每句破折号数", "破折号数占比", "平均每句上引号数", "上引号数占比", "平均每句下引号数", "下引号数占比", "平均每句上圆括号数", "上圆括号数占比", "平均每句下圆括号数", "下圆括号数占比", "平均每句上方括号数", "上方括号数占比", "平均每句下方括号数", "下方括号数占比", "平均每段字符数", "平均每段词数", "平均每段句数", "平均每句单字重复率", "平均每句双字重复率", "平均每句三字重复率", "平均每句实词数", "平均每句虚词数", "平均每句形容词数", "形容词占比", "平均每句介词数", "介词占比", "平均每句副词数", "副词占比", "平均每句名词数", "名词占比", "平均每句动词数", "动词占比", "平均每句助动词数", "助动词占比", "平均每句连词数", "连词占比", "平均每句并列连词数", "并列连词占比", "平均每句限定词数", "限定词占比", "平均每句感叹词数", "感叹词占比", "平均每句数词数", "数词占比", "平均每句PART数", "PART占比", "平均每句代词数", "代词占比", "平均每句专有名词数", "专有名词占比", "平均每句标点符号数", "标点符号占比", "平均每句从属连词数", "从属连词占比", "平均每句特殊符号数", "特殊符号占比", "平均每句其他词性", "其他词性占比", "平均每句空格", "空格占比", "平均每句实体数", "重复过的实体占所有独特实体数的比率", "平均每句人名实体数", "人名实体占比", "平均每句国家/宗教/政治团体实体数", "国家/宗教/政治团体实体占比", "平均每句设施实体数", "设施实体占比", "平均每句组织实体数", "组织实体占比", "平均每句GPE实体数", "GPE实体占比", "平均每句地点实体数", "地点实体占比", "平均每句产品实体数", "产品实体占比", "平均每句事件实体数", "事件实体占比", "平均每句艺术作品实体数", "艺术作品实体占比", "平均每句法律实体数", "法律实体占比", "平均每句语言实体数", "语言实体占比", "平均每句日期实体数", "日期实体占比", "平均每句时间实体数", "时间实体占比", "平均每句百分比实体数", "百分比实体占比", "平均每句货币实体数", "货币实体占比", "平均每句数量实体数", "数量实体占比", "平均每句序数实体数", "序数实体占比", "平均每句基数实体数", "基数实体占比", "主-主实体转移", "主-宾实体转移", "主-其他实体转移", "主-无实体转移", "宾-主实体转移", "宾-宾实体转移", "宾-其他实体转移", "宾-无实体转移", "其他-主实体转移", "其他-宾实体转移", "其他-其他实体转移", "其他-无实体转移", "无-主实体转移", "无-宾实体转移", "无-其他实体转移", "无-无实体转移"])
 const result = ref({})
 // const result = ref({
 //     "idx": 0,
@@ -400,41 +401,58 @@ const ragDocProc = (doc) => {
           </template>
           {{ ragDocProc(doc) }}
         </el-descriptions-item>
-        <div v-for="feature in result['explain_output']" :key="feature">
-          <el-descriptions-item span="2">
-            <template #label>
-              <div class="cell-item">
-                <el-icon style="margin-right: 8px">
-                  <setting />
-                </el-icon>
-                {{ feature['特征名'] }}
-              </div>
-            </template>
-            {{ feature['特征值'] }}
-          </el-descriptions-item>
-          <el-descriptions-item span="2">
-            <template #label>
-              <div class="cell-item">
-                <el-icon style="margin-right: 8px">
-                  <odometer />
-                </el-icon>
-                重要性
-              </div>
-            </template>
-            {{ feature['重要性'] }}
-          </el-descriptions-item>
-          <el-descriptions-item span="2">
-            <template #label>
-              <div class="cell-item">
-                <el-icon style="margin-right: 8px">
-                  <odometer />
-                </el-icon>
-                常模
-              </div>
-            </template>
-            {{ feature['常模'] }}
-          </el-descriptions-item>
-        </div>
+        <el-descriptions-item span="6">
+          <template #label>
+            <div class="cell-item">
+              <el-icon style="margin-right: 8px">
+                <odometer />
+              </el-icon>
+              语言学特征
+            </div>
+          </template>
+          <el-descriptions
+            style="margin: -12px -15px;"
+            :column="6"
+            size="large"
+            border
+          >
+            <div v-for="feature in result['explain_output']" :key="feature">
+              <el-descriptions-item span="2">
+                <template #label>
+                  <div class="cell-item">
+                    <el-icon style="margin-right: 8px">
+                      <setting />
+                    </el-icon>
+                    {{ featureDesc[parseInt(feature['特征名'].split("feature_")[1])] }}
+                  </div>
+                </template>
+                {{ feature['特征值'] }}
+              </el-descriptions-item>
+              <el-descriptions-item span="2">
+                <template #label>
+                  <div class="cell-item">
+                    <el-icon style="margin-right: 8px">
+                      <odometer />
+                    </el-icon>
+                    重要性
+                  </div>
+                </template>
+                {{ feature['重要性'] }}
+              </el-descriptions-item>
+              <el-descriptions-item span="2">
+                <template #label>
+                  <div class="cell-item">
+                    <el-icon style="margin-right: 8px">
+                      <odometer />
+                    </el-icon>
+                    常模
+                  </div>
+                </template>
+                {{ feature['常模'] }}
+              </el-descriptions-item>
+            </div>
+          </el-descriptions>
+        </el-descriptions-item>
         <el-descriptions-item v-show="false"></el-descriptions-item>
       </el-descriptions>
     </div>
